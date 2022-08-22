331ec2b750900e575f9d189665b0dbbdea211cfc
SnowFall98 <yordymora98@gmail.com>
2022-08-22 15:51:21 -0500

    Correción de variable de DB

--- database/db.ts
+++ database/db.ts
@@ -7,21 +7,21 @@ import mongoose from 'mongoose';
  * 3 = disconnecting
  */
 
-const mongooConnection = {
+const mongoConnection = {
     isConnected: 0
 }
 
 export const connect = async() =>{
 
-    if(mongooConnection.isConnected ){
+    if(mongoConnection.isConnected ){
         console.log('Estamos conectados');
         return;
     }
 
     if(mongoose.connections.length > 0){
-        mongooConnection.isConnected = mongoose.connections[0].readyState;
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
 
-        if(mongooConnection.isConnected === 1){
+        if(mongoConnection.isConnected === 1){
             console.log('Usando anterior conexion');
             return;
         }
@@ -31,7 +31,7 @@ export const connect = async() =>{
 
 
     await mongoose.connect(process.env.MONGO_URL || '');
-    mongooConnection.isConnected =1;
+    mongoConnection.isConnected =1;
     console.log('conectado a MongoDB:', process.env.MONGO_URL);
 }
 
@@ -43,10 +43,10 @@ export const disconnect = async () => {
     if( process.env.NODE_ENV === 'development') return;
     
     
-    if(mongooConnection.isConnected === 0) return;
+    if(mongoConnection.isConnected === 0) return;
     
     await mongoose.disconnect();
-    mongooConnection.isConnected =0;
+    mongoConnection.isConnected =0;
     console.log('Desconectado');
 
 }
